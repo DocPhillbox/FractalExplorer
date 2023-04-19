7909b4539cd28e5f2fb3d1e2a3b83c5fb8db60a8
Andrey Bastien <71886221+DocPhillbox@users.noreply.github.com>
2023-04-19 08:25:05 +0200

    [mandelbrot] Add thread for rendering fractal

--- MandelbrotWidget.cpp
+++ MandelbrotWidget.cpp
@@ -1,40 +1,70 @@
 #include <QPainter>
 #include "MandelbrotWidget.h"
 
-MandelbrotWidget::MandelbrotWidget(QWidget *parent) :
-        QWidget(parent)
+const double DefaultCenterX = -0.637011;
+const double DefaultCenterY = -0.0395159;
+const double DefaultScale = 0.00403897;
+
+const double ZoomInFactor = 0.6;
+const double ZoomOutFactor = 1 / ZoomInFactor;
+const int ScrollStep = 20;
+
+MandelbrotWorker::MandelbrotWorker(QObject *parent)
+    : QThread(parent)
 {
 
 }
 
-void MandelbrotWidget::paintEvent(QPaintEvent *event)
+void MandelbrotWorker::render(double centerX, double centerY, double scaleFactor, int width, int height, double devicePixelRatio)
 {
-    QPainter painter(this);
-    int width = this->width();
-    int height = this->height();
-    int iterations = 255;
-    double minR = -1.5;
-    double maxR = 0.7;
-    double minI = -1.0;
-    double maxI = 1.0;
+    this->centerX = centerX;
+    this->centerY = centerY;
+    this->scaleFactor = scaleFactor;
+    this->width = width;
+    this->height = height;
+    this->devicePixelRatio = devicePixelRatio;
 
-    for (int y = 0; y < height; y++)
+
+    draw();
+}
+
+void MandelbrotWorker::draw()
+{
+    printf("run");
+    QImage image(QSize(width, height), QImage::Format_RGB32);
+    int halfWidth = this->width / 2;
+    int halfHeight = this->height / 2;
+    image.setDevicePixelRatio(devicePixelRatio);
+    int iterations = 26;
+    double minR = -3;
+    double maxR = 2.5;
+    double minI = -1.5;
+    double maxI = 1.5;
+
+    for (int y = 0; y < height; ++y)
     {
-        for (int x = 0; x < width; x++)
+        auto scaneLine = reinterpret_cast<uint *>(image.scanLine(y));
+
+        for (int x = 0; x < width; ++x)
         {
-            double cr = mapToReal(x, width, minR, maxR);
-            double ci = mapToImaginary(y, height, minI, maxI);
+            // double cr = mapToReal(x, width, minR, maxR);
+            // double ci = mapToImaginary(y, height, minI, maxI);
+            double cr = centerX + ((x - halfWidth) * scaleFactor);
+            double ci = centerY + ((y - halfHeight) * scaleFactor);
             int n = isStable(cr, ci, iterations);
-            int r = ((int) (n * sin(n)) % 256);
+            int r = (n % 256);
             int g = (n % 256);
             int b = ((int)pow(n, 2.0) % 256);
-            painter.setPen(QColor(r, g, b));
-            painter.drawPoint(x, y);
+            if (n < iterations)
+                *scaneLine++ = qRgb(r, g, b);
+            else
+                *scaneLine++ = qRgb(0, 0, 0);
         }
     }
+    emit renderedImage(image, scaleFactor);
 }
 
-int MandelbrotWidget::isStable(double cr, double ci, int iterations)
+int MandelbrotWorker::isStable(double cr, double ci, int iterations)
 {
     int i = 0;
     double zr = 0.0; double zi = 0.0;
@@ -48,14 +78,83 @@ int MandelbrotWidget::isStable(double cr, double ci, int iterations)
     return i;
 }
 
-double MandelbrotWidget::mapToReal(int x, int width, double minR, double maxR)
+double MandelbrotWorker::mapToReal(double x, int width, double minR, double maxR)
 {
     double range = maxR - minR;
     return x * (range / width) + minR;
 }
 
-double MandelbrotWidget::mapToImaginary(int y, int height, double minI, double maxI)
+double MandelbrotWorker::mapToImaginary(double y, int height, double minI, double maxI)
 {
     double range = maxI - minI;
     return y * (range / height) + minI;
+}
+
+MandelbrotWidget::MandelbrotWidget(QWidget *parent)
+    : QWidget(parent),
+    centerX(DefaultCenterX),
+    centerY(DefaultCenterY),
+    pixmapScale(DefaultScale),
+    curScale(DefaultScale)
+{
+    connect(&workerThread, &MandelbrotWorker::renderedImage, this, &MandelbrotWidget::updatePixmap);
+}
+
+void MandelbrotWidget::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    workerThread.render(this->centerX, this->centerY, this->curScale, this->width(), this->height(), devicePixelRatio());
+    QRectF exposed = painter.transform().inverted().mapRect(rect()).adjusted(-1, -1, 1, 1);
+    painter.drawPixmap(exposed, pixmap, exposed);
+}
+
+void MandelbrotWidget::updatePixmap(const QImage &image, double scaleFactor)
+{
+    pixmap = QPixmap::fromImage(image);
+    update();
+}
+
+void MandelbrotWidget::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key()) {
+        case Qt::Key_Z:
+            zoom(ZoomInFactor);
+            break;
+        case Qt::Key_U:
+            zoom(ZoomOutFactor);
+            break;
+        case Qt::Key_Left:
+            scroll(-ScrollStep, 0);
+            break;
+        case Qt::Key_Right:
+            scroll(+ScrollStep, 0);
+            break;
+        case Qt::Key_Up:
+            scroll(0, ScrollStep);
+            break;
+        case Qt::Key_Down:
+            scroll(0, -ScrollStep);
+            break;
+        case Qt::Key_Q:
+            close();
+            break;
+        default:
+            QWidget::keyPressEvent(event);
+    }
+}
+
+void MandelbrotWidget::zoom(double zoomFactor)
+{
+    printf("Zoom");
+    curScale *= zoomFactor;
+    update();
+    workerThread.render(centerX, centerY, curScale, size().width(), size().height(), devicePixelRatio());
+}
+
+void MandelbrotWidget::scroll(int deltaX, int deltaY)
+{
+    centerX += deltaX * curScale;
+    centerY += deltaY * curScale;
+    update();
+    workerThread.render(centerX, centerY, curScale, size().width(), size().height(), devicePixelRatio());
 }
--- MandelbrotWidget.h
+++ MandelbrotWidget.h
@@ -1,23 +1,60 @@
 #include <QWidget>
+#include <QThread>
+#include <QKeyEvent>
 
 #ifndef FRACTALEXPLORER_MANDELBROTWIDGET_H
 #define FRACTALEXPLORER_MANDELBROTWIDGET_H
+
+class MandelbrotWorker : public QThread
+{
+    Q_OBJECT
+
+public:
+    explicit MandelbrotWorker(QObject *parent = nullptr);
+
+    void render(double centerX, double centerY, double scaleFactor, int width, int height, double devicePixelRatio);
+    void draw();
+
+    signals:
+        void renderedImage(const QImage &image, double scaleFactor);
+
+private:
+    double centerX;
+    double centerY;
+    double scaleFactor;
+    int width;
+    int height;
+    double devicePixelRatio;
+
+    int isStable(double cr, double ci, int iterations);
+    static double mapToReal(double x, int width, double minR, double maxR);
+    static double mapToImaginary(double y, int height, double minI, double maxI);
+};
 
 class MandelbrotWidget : public QWidget
 {
     Q_OBJECT
 
 public:
-    MandelbrotWidget(QWidget *parent = nullptr);
+    explicit MandelbrotWidget(QWidget *parent = nullptr);
 
 protected:
     void paintEvent(QPaintEvent *event) override;
-    int isStable(double cr, double ci, int iterations);
-    double mapToReal(int x, int width, double minR, double maxR);
-    double mapToImaginary(int y, int height, double minI, double maxI);
 
 private:
+    QPixmap pixmap;
+    MandelbrotWorker workerThread;
+    double centerX;
+    double centerY;
+    double pixmapScale;
+    double curScale;
 
+    void keyPressEvent(QKeyEvent *event) override;
+    void scroll(int deltaX, int deltaY);
+
+    private slots:
+        void updatePixmap(const QImage &image, double scaleFactor);
+        void zoom(double zoomFactor);
 };
 
 #endif //FRACTALEXPLORER_MANDELBROTWIDGET_H
